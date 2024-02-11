3cdbaed4805fcf78e10925f504f747bc1353124d
ygursu <yasemingrs@gmail.com>
2024-02-11 23:29:20 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp57/Program.cs
@@ -0,0 +1,30 @@
+﻿// using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace trafiklambasiswitchcase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            Console.WriteLine("Lütfen trafik lambası rengini giriniz");
+            string renk = Console.ReadLine();
+
+            switch (renk)
+            {
+                case "kırmızı": Console.WriteLine("Dur"); break;
+                case "sarı": Console.WriteLine("Hazır Ol"); break;
+                case "yeşil": Console.WriteLine("İlerle"); break;
+
+                default: Console.WriteLine("Böyle bir trafik rengi yok"); break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
